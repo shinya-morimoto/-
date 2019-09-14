bb7fe8b105ffe0ed2970536b8d7a449f9e26d59f
EC2 Default User <ec2-user@ip-172-31-21-162.us-east-2.compute.internal>
2019-09-14 11:36:41 +0000

    destoy追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190831084033) do
+ActiveRecord::Schema.define(version: 20190909142239) do
 
   create_table "sample_apps", force: :cascade do |t|
     t.string "name"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20190831084033) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
